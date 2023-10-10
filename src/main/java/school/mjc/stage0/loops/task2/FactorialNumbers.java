79d2f85fddcdb50a3f8427141b9df85a1959e35e
Leclassique <75534147+Leclassique@users.noreply.github.com>
2023-10-10 23:18:24 +0300

    Completed task

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,6 +2,12 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
-
+        int number = 0;
+        int factorial = 1;
+        while (factorial <= printToInclusive) {
+            System.out.println(factorial);
+            number++;
+            factorial = factorial * number;
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,5 +2,19 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+        int number = 0;
+        while (number <= printToInclusive) {
+            boolean isPrime = true;
+            for (int i = 0; i <= Math.sqrt(number); i++) {
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                System.out.println(number);
+            }
+            number++;
+        }
     }
 }
